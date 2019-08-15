53f5056ca1b858e57eb40590615275324da030d5
brgd <hohhots@qq.com>
2019-08-15 14:52:34 +0800

    or book pages with pageNum_ASC

--- src/resolvers/Book.js
+++ src/resolvers/Book.js
@@ -3,7 +3,9 @@ function postedBy(parent, args, context) {
 }
 
 function pages(parent, args, context) {
-  return context.prisma.book({ id: parent.id }).pages();
+  return context.prisma
+    .book({ id: parent.id })
+    .pages({ orderBy: 'pageNum_ASC' });
 }
 
 module.exports = {
