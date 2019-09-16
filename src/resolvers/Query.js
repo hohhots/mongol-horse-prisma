1ad6019f4dfdd0286d86d9dd8998e2d960ec58c0
brgd <hohhots@qq.com>
2019-09-17 00:09:53 +0800

    add query for page

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,34 @@
+async function page(parent, args, context) {
+  return await context.prisma.page({ id: args.pageId });
+}
+
 async function book(parent, args, context) {
   return await context.prisma.book({ id: args.bookId });
+}
+
+async function pageList(parent, args, context) {
+  const where = args.filter
+    ? {
+        content_contains: args.filter,
+      }
+    : {};
+
+  const pages = await context.prisma.pages({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  });
+  const count = await context.prisma
+    .pagesConnection({
+      where,
+    })
+    .aggregate()
+    .count();
+  return {
+    pages,
+    count,
+  };
 }
 
 async function bookList(parent, args, context) {
@@ -30,33 +59,9 @@ async function bookList(parent, args, context) {
   };
 }
 
-async function pageList(parent, args, context) {
-  const where = args.filter
-    ? {
-        content_contains: args.filter,
-      }
-    : {};
-
-  const pages = await context.prisma.pages({
-    where,
-    skip: args.skip,
-    first: args.first,
-    orderBy: args.orderBy,
-  });
-  const count = await context.prisma
-    .pagesConnection({
-      where,
-    })
-    .aggregate()
-    .count();
-  return {
-    pages,
-    count,
-  };
-}
-
 module.exports = {
+  page,
   book,
-  bookList,
   pageList,
+  bookList,
 };
