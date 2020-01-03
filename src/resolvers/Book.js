d07a604c3c650303bfe18ac6eaa615abd0fd599e
brgd <hohhots@qq.com>
2020-01-03 22:52:00 +0800

    add book and pages filter

--- src/resolvers/Book.js
+++ src/resolvers/Book.js
@@ -2,10 +2,11 @@ function postedBy(parent, args, context) {
   return context.prisma.book({ id: parent.id }).postedBy();
 }
 
-function pages(parent, args, context) {
-  return context.prisma
-    .book({ id: parent.id })
-    .pages({ orderBy: 'pageNum_ASC' });
+function pages(parent, args, context, info) {
+  return context.prisma.book({ id: parent.id }).pages({
+    where: { content_contains: info.variableValues.filter },
+    orderBy: 'pageNum_ASC',
+  });
 }
 
 module.exports = {
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -7,27 +7,54 @@ async function book(parent, args, context) {
 }
 
 async function bookList(parent, args, context) {
-  const where = args.filter
-    ? {
-        OR: [
-          { title_contains: args.filter },
-          { preview_contains: args.filter },
-        ],
-      }
-    : {};
-
   const books = await context.prisma.books({
-    where,
     skip: args.skip,
     first: args.first,
     orderBy: args.orderBy,
   });
+  const count = await context.prisma
+    .booksConnection()
+    .aggregate()
+    .count();
+  return {
+    books,
+    count,
+  };
+}
+
+async function bookFilterList(parent, args, context) {
+  const fragment = `
+    fragment BookWithPages on Book {
+      id
+      title
+      preview
+    }
+  `;
+
+  const where = {
+    OR: [
+      { title_contains: args.filter },
+      { preview_contains: args.filter },
+      { pages_some: { content_contains: args.filter } },
+    ],
+  };
+
+  const books = await context.prisma
+    .books({
+      where,
+      skip: args.skip,
+      first: args.first,
+      orderBy: args.orderBy,
+    })
+    .$fragment(fragment);
+
   const count = await context.prisma
     .booksConnection({
       where,
     })
     .aggregate()
     .count();
+
   return {
     books,
     count,
@@ -38,4 +65,5 @@ module.exports = {
   page,
   book,
   bookList,
+  bookFilterList,
 };
